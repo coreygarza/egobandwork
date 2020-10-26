3a95268f1205852639154324b84b28070590eb74
coreygold888 <coreygold888@gmail.com>
2020-10-26 16:49:43 -0400

    5 solution template creation

--- solutions.php
+++ solutions.php
@@ -9,11 +9,25 @@
 global $post;
 
 $context = \Timber\Timber::get_context();
-$context['userIp'] = $_SERVER['REMOTE_ADDR'];
+// $context['userIp'] = $_SERVER['REMOTE_ADDR'];
 
-$theme = new \Chisel\Extensions\Theme();
-$context['productStandard'] = $theme->getProductData('standard');
-$context['productRooms'] = $theme->getProductData('rooms');
-$context['productParcels'] = $theme->getProductData('parcels');
+// $theme = new \Chisel\Extensions\Theme();
+// $context['productStandard'] = $theme->getProductData('standard');
+// $context['productRooms'] = $theme->getProductData('rooms');
+// $context['productParcels'] = $theme->getProductData('parcels');
 
-\Timber\Timber::render( 'layouts/solutions.twig', $context );
+
+
+$enSolusionsName = [
+    'hotels-and-resorts' => 'hoteles-y-resorts',
+    'theme-and-water-parks' => 'parques-tematicos',
+    'music-festivals' => 'festivales-de-musica',
+    'food-and-beverage-festivals' => 'eventos-gastronomicos',
+    'sporting-events' => 'recintos-deportivos'
+];
+$enSolusionsName = $lang == 'en' ? $enSolusionsName[$post->post_name] : $post->post_name;
+
+
+\Timber\Timber::render( array( 'solutions-' . $enSolusionsName . '.twig', 'solutions.twig' ), $context );
+
+// \Timber\Timber::render( 'layouts/solutions.twig', $context );
